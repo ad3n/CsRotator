6bca1a755db2b5a568c203b38beca7863554c920
aden <surya.kejawen@gmail.com>
2018-08-07 16:56:14 +0700

    improvement

--- src/Controller/Admin/ReportController.php
+++ src/Controller/Admin/ReportController.php
@@ -4,26 +4,33 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Repository\CampaignContacVisitRepository;
 use App\Security\Permission\Permission;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/reports")
  *
- * @Permission(menu="REPORT")
+ * @Permission(menu="CAMPAIGN")
  *
  * @author Muhamad Surya Iksanudin <surya.iksanudin@gmail.com>
  */
-class ReportController
+class ReportController extends Controller
 {
     /**
      * @Route("/campaign/{slug}", methods={"GET"}, name="report_campaigns", options={"expose"=true})
      *
      * @Permission(actions=Permission::VIEW)
      */
-    public function index(Request $request)
+    public function index(string $slug, Request $request, CampaignContacVisitRepository $repository)
     {
+        $startDate = \DateTime::createFromFormat('Y-m-d', $request->query->get('startDate', date('Y-m-01')));
+        $endDate = \DateTime::createFromFormat('Y-m-d', $request->query->get('endDate', date('Y-m-t')));
 
+        $statistic = $repository->getStatistic($slug, $startDate, $endDate);
+
+        return $this->render('report/campaign.html.twig', ['title' => 'Laporan Program/Campaign', 'data' => $statistic]);
     }
 }
--- src/Repository/CampaignContacVisitRepository.php
+++ src/Repository/CampaignContacVisitRepository.php
@@ -7,6 +7,7 @@ namespace App\Repository;
 use App\Entity\CampaignContactVisit;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 /**
  * @author Muhamad Surya Iksanudin <surya.iksanudin@gmail.com>
@@ -16,5 +17,29 @@ class CampaignContacVisitRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, CampaignContactVisit::class);
+    }
+
+    public function getStatistic(string $slug, \DateTime $startDate, \DateTime $endDate)
+    {
+        $rsm = new ResultSetMapping();
+        $rsm->addScalarResult('tanggal', 'tanggal');
+        $rsm->addScalarResult('kunjungan', 'kunjungan');
+
+        $query = $this->_em->createNativeQuery(sprintf("
+            SELECT 
+                DATE(ccv.visit_time) AS tanggal, 
+                COUNT(ccv.visit_time) AS kunjungan 
+            FROM 
+                campaign_contact_visits ccv
+            INNER JOIN
+                campaign_contacts cc ON ccv.campaign_contact_id = cc.id
+            INNER JOIN
+                campaigns c ON cc.campaign_id = c.id
+            WHERE
+                c.slug = '%s' AND DATE(ccv.visit_time) >= '%s' AND DATE(ccv.visit_time) <= '%s'
+            GROUP BY DATE(visit_time);
+        ", $slug, $startDate->format('Y-m-d'), $endDate->format('Y-m-d')), $rsm);
+
+        return $query->getResult();
     }
 }
