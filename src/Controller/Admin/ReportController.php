9d7a46b63b033f4863b4c217579495b3d2088746
aden <surya.kejawen@gmail.com>
2018-08-07 14:28:36 +0700

    prepare chart

---
+++ src/Controller/Admin/ReportController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Security\Permission\Permission;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/reports")
+ *
+ * @Permission(menu="REPORT")
+ *
+ * @author Muhamad Surya Iksanudin <surya.iksanudin@gmail.com>
+ */
+class ReportController
+{
+
+}
